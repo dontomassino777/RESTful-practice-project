cba600807fc830e086cd83eeeaaf30ce6c555ee3
Tomas Fisher <dontomassino777@gmail.com>
2022-04-04 11:40:46 -0600

    mongodb wired up

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const bookRouter = express.Router();
+const port = process.env.PORT || 3000;
+const Book = require('./models/bookmodel');
 
-var port = process.env.PORT || 3000;
+bookRouter.route('/books')
+    .get((req, res) => {
+        Book.find((err, books) => {
+            if(err) {
+                return res.send(err);
+            }
+            return res.json(books);
+        });
+    });
+app.use('/api', bookRouter);
 
 app.get('/', (req,res)=> {
     res.send('Welcome to my API');
